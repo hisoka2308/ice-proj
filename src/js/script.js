8d39f4ac686b7005bc98cf5dc50ba3e89fb75036
hisoka2308 <gulirausenlieva78@gmail.com>
2022-05-26 10:10:19 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        dots:true,
+        // nav: true,
+    });
+});
